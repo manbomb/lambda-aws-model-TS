09d7617b9e6ec227ac6b581d61b186207a5ad07b
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-07-27 09:05:22 -0300

    init: batman

---
+++ README.md
@@ -0,0 +1,1 @@
+https://y954ovpfig.execute-api.us-east-1.amazonaws.com
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from "aws-lambda"
+import Router from "lambda-router-typescript";
+
+type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2
+type LambdaFunctionUrlEvent = APIGatewayProxyEventV2
+
+export async function handler(event: LambdaFunctionUrlEvent): Promise<LambdaFunctionUrlResult> {
+
+    const router = new Router();
+
+    router.get('/', async (event) => ({ message: 'Oi!', event }));
+
+    try {
+        const response = await router.call(event);
+        return response
+    } catch (error: any) {
+        return {
+            statusCode: error.statusCode || 500,
+            body: JSON.stringify(error.body) || 'Internal error.',
+        }
+    }
+}
